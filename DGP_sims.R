b1cb82c3b40a26535deb114fd493e880a0f2e502
Steve Walsh <stevewalsh124@yahoo.com>
2022-01-28 10:24:19 -0500

    initial playing with Deep GP sims

---
+++ DGP_sims.R
@@ -0,0 +1,35 @@
+############################################
+# Simulating deep GPs, naively
+# Steve Walsh
+# January 28 2022
+############################################
+
+# Based on Bobby Gramacy's "Surrogates", Chapter 5
+
+n <- 500
+X <- matrix(seq(0, 10, length=n), ncol=1)
+
+library(plgp) #plgp::distance
+library(mvtnorm)
+
+#squared distance
+D <- distance(X) 
+
+# lengthscale = 1
+# marginal variance = 1
+eps <- sqrt(.Machine$double.eps) 
+Sigma <- exp(-D) + diag(eps, n)
+
+# first layer
+W <- c(rmvnorm(1, sigma=Sigma))
+
+# second layer
+D2 <- distance(c(W))
+Sigma_2 <- exp(-D2/0.3) + diag(eps, n)
+Y <- rmvnorm(1, sigma=Sigma_2) #mean=W,
+
+#Plot each combination of layers/output
+par(mfrow=c(2,2))
+plot(X, W, type="l"); abline(h=0, col=2, lwd=2, lty=2)
+plot(W, Y, type = "l"); abline(h=0, col=2, lwd=2, lty=2)
+plot(X, Y, type="l"); abline(h=0, col=2, lwd=2, lty=2)
