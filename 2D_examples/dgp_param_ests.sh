e1654e32f68cd320784808bb9357fd4fb5f1c40b
Steve Walsh <walsh124@vt.edu>
2022-01-31 21:48:30 -0500

    bash script for looping through DGP estimation of storms

---
+++ 2D_examples/dgp_param_ests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Make sure to change the -A line to your allocation
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=2
+#SBATCH -t 24:00:00
+#SBATCH -p normal_q
+#SBATCH -A Precipit
+
+# Add modules
+module purge
+module load singularity
+
+# Change to the directory from which the job was submitted
+#cd $PBS_O_WORKDIR
+
+# Run R
+
+
+i=${1:-1}
+#j=${2:-0}
+
+#nohup R CMD BATCH "--args seed=$i reps=$reps" spam_mc.R spam_mc_$i.Rout &
+
+echo "$( date ): Starting TC $i now"
+singularity exec --bind=/groups:/groups /groups/arcsingularity/ood-rstudio-geospatial_4.0.3.sif R CMD BATCH --no-restore "--args ste=$i" /home/walsh124/dgptc/2D_examples/2D_TCexample.R Rout/prediction${i}.Rout 
+echo "$( date ): Finished prediction $i" 
+
+exit
