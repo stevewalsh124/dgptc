13f0e4a6dd92a07a0c890d96f69200a88cd25338
Steve Walsh <walsh124@vt.edu>
2022-02-10 12:13:11 -0500

    loop over all TCs; add args

--- 2D_examples/deform.R
+++ 2D_examples/deform.R
@@ -6,47 +6,51 @@
 
 library(Morpho)
 
-FL <- read.csv("~/NAM-Model-Validation/csv/stormsFL.csv", row.names = 1)$x
-fl <- FL[-which(FL %in% c(5,10,29))]
+niters <- 50000
+k <- 20   # show every kth deformation
+pmx <- T  # use MCMCs with Prior Mean X for hidden layer?
 
-ste <- 4
-niters <- 25000
-pmx <- F
+pdf("pdf/deform/1to18_every20th.pdf")
 
-# load in training points, deformed points, param chains, etc
-load(paste0("rda/FL_fits/storm",ste,"_niters",niters,
-            "krig",if(pmx){"pmx"},".rda"))
+for (ste in 1:18) {
+  
+  # load in training points, deformed points, param chains, etc
+  load(paste0("rda/FL_fits/storm",ste,"_niters",niters,
+              "krig",if(pmx){"pmx"},".rda"))
+  
+  # the deformation often negates some of the values; remove this
+  fit$ww <- list()
+  for (i in 1:length(fit$g)) {
+    fit$ww[[i]] <- ifelse(fit$w[[i]] < 0, -fit$w[[i]], fit$w[[i]])
+  }
+  
+  # # Basic comparison of the lat/lon and deformed locations 
+  # pdf("pdf/deform.pdf")
+  # par(mfrow=c(2,2))
+  # plot(fit$x)
+  # plot(rasterFromXYZ(cbind(fit$x, fit$y)))
+  # plot(fit$ww[[5]])
+  # plot(fit$ww[[50000]])
+  # 
+  # par(mfrow=c(1,1))
+  # for (i in 1:(100)) { plot(fit$ww[[i*500]], main = i*500) }
+  # dev.off()
+  
+  
+  # These two show a grid warped based on thin plate splines
+  
+  # # focus on deformations early on in the chain; every k'th
+  # pdf(paste0("pdf/deform/",ste,if(pmx){"pmx"},".pdf"))
+  for(i in 1:20){ deformGrid2d(fit$x, fit$ww[[k*(i-1)+1]],
+                               ngrid=25, pch=19, main=paste(ste, k*(i-1)+1), gridcol = "black") }
+  # dev.off()
+  
+  # look at more of big picture changes in the chain; 20 images over the chain
+  # pdf(paste0("pdf/deform/",ste,if(pmx){"pmx"},"_all.pdf"))
+  # for(i in 1:20){ deformGrid2d(fit$x, fit$ww[[niters/20*(i-1)+1]],
+  #                              ngrid=25, pch=19, main=paste(ste, niters/20*(i-1)+1), gridcol = "black") }
+  # dev.off()
 
-# the deformation often negates some of the values; remove this
-fit$ww <- list()
-for (i in 1:length(fit$g)) {
-  fit$ww[[i]] <- ifelse(fit$w[[i]] < 0, -fit$w[[i]], fit$w[[i]])
 }
 
-# # Basic comparison of the lat/lon and deformed locations 
-# pdf("pdf/deform.pdf")
-# par(mfrow=c(2,2))
-# plot(fit$x)
-# plot(rasterFromXYZ(cbind(fit$x, fit$y)))
-# plot(fit$ww[[5]])
-# plot(fit$ww[[50000]])
-# 
-# par(mfrow=c(1,1))
-# for (i in 1:(100)) { plot(fit$ww[[i*500]], main = i*500) }
-# dev.off()
-
-
-# These two show a grid warped based on thin plate splines
-
-# focus on deformations early on in the chain; every 50th
-pdf(paste0("pdf/deform/",ste,if(pmx){"pmx"},".pdf"))
-for(i in 1:20){ deformGrid2d(fit$x, fit$ww[[50*(i-1)+1]],
-                             ngrid=25, pch=19, main=50*(i-1)+1, gridcol = "black") }
 dev.off()
-
-# look at more of big picture changes in the chain; 20 images over the chain
-pdf(paste0("pdf/deform/",ste,if(pmx){"pmx"},"_all.pdf"))
-for(i in 1:20){ deformGrid2d(fit$x, fit$ww[[niters/20*(i-1)+1]],
-                             ngrid=25, pch=19, main=niters/20*(i-1)+1, gridcol = "black") }
-dev.off()
-
