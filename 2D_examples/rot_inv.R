3fc3144be49d56571286e860c77d3edd6328792b
Steve Walsh <walsh124@vt.edu>
2022-02-14 17:21:10 -0500

    fix rotation invariance for deformation comparison

--- 2D_examples/rot_inv.R
+++ 2D_examples/rot_inv.R
@@ -1,99 +1,85 @@
 #############################################
 # Make rotation-invariant deformation plots #
+# Steve Walsh                               #
+# February 14 2022                          #
 #############################################
+
+library(Morpho) #deformGrid2d
+
+k <- 5000 # every kth warping to plot
+tp <- 10 # total plots to produce (excluding original)
 
 pdf("pdf/deform/deform_compare.pdf")
 
-for (ste in c(1,3,5,6,7,8,10,11,12,13,15,16,17)) {
-  load(paste0("~/dgptc/2D_examples/rda/FL_fits/storm",ste,"_niters5000krigpmx.rda"))
+# Loop over TCs
+for (ste in 1:18) {
+  load(paste0("rda/FL_fits/storm",ste,"_niters75000krigpmx.rda")) #loads fit
   
-  X <- fit$x
-  
-  k <- 100 # every kth warping to plot
-  tp <- 10 # total plots to produce 
+  x <- t(fit$x)
   
   # the deformation often negates some of the values; remove negatives
-  fit$ww <- list()
-  for (i in 1:length(fit$g)) {
-    fit$ww[[i]] <- ifelse(fit$w[[i]] < 0, -fit$w[[i]], fit$w[[i]])
-  }
-  W <- fit$ww
+  # this removes any reflections incurred within the hidden layer
+  W <- list()
+  for (i in 1:fit$nmcmc) W[[i]] <- ifelse(fit$w[[i]] < 0, -fit$w[[i]], fit$w[[i]])
+
+  # The original non-invariant plots; shown in left side for comparison
+  par(mfrow=c(1,1))
+  deformGrid2d(fit$x, W[[1]], ngrid=25, pch=19, main=paste(ste, 1), gridcol = "black")
   
-  library(Morpho)
+  # Get reference points and scale
+  load("rda/FL_ref.rda")
+  zz <- FL_ref[1,]
+  zo <- FL_ref[2,]
+  points(zz[1],zz[2], lwd=3) # add to plot above
+  points(zo[1],zo[2], lwd=3) # add to plot above
   
-  # The original non-invariant plots; shown in left side below for comparison
-  for (i in 1) { #1:tp
-    deformGrid2d(fit$x, fit$ww[[k*(i-1)+1]],
-                 ngrid=25, pch=19, main=paste(ste, k*(i-1)+1), gridcol = "black")
-  }
+  # this finds the length of the original reference vector
+  zzo <- zz - zo
+  ref.scale <- sqrt(t(zzo)%*%zzo)
   
-  # The Xs beforehand
-  # plot(X, asp = 1, pch=".")
-  # plot(W[[4]], asp = 1, pch=".")
+  # establish which points in x (and y) are reference points
+  # which low (wl) is (0,0) and which high (wh) is (0,1)
+  wl <- which(abs(x[1,]-FL_ref[1,1]) < 1e-3 & abs(x[2,]-FL_ref[1,2]) < 1e-3) # (0,0) point
+  wh <- which(abs(x[1,]-FL_ref[2,1]) < 1e-3 & abs(x[2,]-FL_ref[2,2]) < 1e-3) # (0,1) point
   
-  # (1) Call a point in the southern border of Florida point (0,0);
-  # in this storm, find first (and only for this TC) point with y=0; this is (0.8,0)
-  # So I will subtract 0.8 from each of these
-  lo_lat <- which(X[,2]==0 & abs(X[,1]-0.7692308) < 1e-3)#which(X[,2]==0)[1]
-  X[lo_lat,]
+  # If reference point pair are not in training set, skip
+  if(length(wl)==0 | length(wh)==0) next
+  print(ste)
   
-  # (2) Find the point on the northern border of Florida with same longitude as (0,0) and call it point (0,1);
-  hi_lat <- which(X[,2]==1 & abs(X[,1]-0.7692308) < 1e-3)
-  X[hi_lat,]
-  
-  # (c) Find the translation that makes (x_1,x_2) into (0,0) (that's easy). 
-  x_adj <- X[lo_lat,1]
-  X[,1] <- X[,1] - x_adj
-  
-  # (3) For each simulated hidden layer: 
+  # For each simulated hidden layer: 
   for(i in k*(1:tp)+1){
+    
     par(mfrow=c(1,2))
+    # On the left: the original (unchanged) deformation
+    deformGrid2d(fit$x, W[[i]], ngrid=25, pch=19, main=paste(ste, i, "old"), gridcol = "black")
     
-    deformGrid2d(fit$x, fit$ww[[i]],
-                 ngrid=25, pch=19, main=paste(ste, i, "old"), gridcol = "black")
-    
-    # i <- 1 (above)
-    #   (a) Find the "deformed point" corresponding to point (0,0), say (x_1,x_2);
-    #   Note: Ws have not had x_adj subtracted from longitudes
-    w <- W[[i]]
-    w[lo_lat,] 
-    
-    # (b) Find the "deformed point" corresponding to point (0,1), say (x_3,x_4);
-    w[hi_lat,]
-    
-    #     (d) Apply that translation to all deformed points. For example, after (x_3,x_4) 
-    #           gets translated, it becomes (x_3-x_1, x_4-x_2);
-    w[,1] <- w[,1] - x_adj
-    # plot(w, asp=1, pch=".")
-    which(X[,1]==0 & X[,2]==0)
-    
-    #     (e) Now use the dot product to compute the angle between the vector that 
-    #           goes from (0,0) to (x_3-x_1, x_4-x_2). Find the corresponding 
-    #           rotation-scale matrix that makes (x_3-x_1, x_4-x_2) into (0,1);
-    u <- X[hi_lat,]
-    v <- w[hi_lat,]
-    
-    # find angle between (0,0) and v will result in arccos(0/0)=NaN
-    # find angle between ~(0,1) and its corresponding deformation 
+    # Take these deformed points (y) and rotate/scale/translate them
+    # to match orig lat/lon points (x) as well as possible
+    y <- t(W[[i]])
+
+    # First: undo rotation
     ang <- function(u,v){acos((t(u)%*%v)/((sqrt(t(u)%*%u) * sqrt(t(v)%*%v))))}
-    
+    u <- x[,wh] - x[,wl]
+    v <- y[,wh] - y[,wl]
     phi <- ang(u,v)
-    rotMtx <- matrix(c(cos(phi),sin(phi),-sin(phi),cos(phi)),2,2)
+    rotMtx <- matrix(c(cos(phi),-sin(phi),sin(phi),cos(phi)),2,2)
+    y.rot <- rotMtx %*% y
     
-    # rotMtx%*%u / v should return two numbers of equal value
-    # they don't, so I tried taking mean, or the max(abs())
-    print(i)
-    print(c(rotMtx%*%u / v))
-    r <- abs(max(rotMtx%*%u / v))
+    # Second: undo scale
+    # find the length of the deformed reference vector
+    y.ref <- y[,wl] - y[,wh]
+    scale.y <- sqrt(t(y.ref)%*%y.ref)
+    # numerator comes from length of vector with points zz, zo
+    scale.back =  c(ref.scale/scale.y)#1/sqrt(sum(y.translate[,2]^2))
+    y.rot.scale = y.rot * scale.back
+
+    # Third: Translate back
+    translation <- y.rot.scale[,wl] - x[,wl]
+    y.rot.scale.tran <- y.rot.scale - translation
+    ww <- t(y.rot.scale.tran)
     
-    #      (f) Apply that rotation-scale matrix to all translated points. 
-    f <- function(u){r*rotMtx %*%u}
-    ww <- t(apply(w, 1, f))
-    ww[,1] <- ww[,1]+x_adj
-    # plot(ww)
-    #      (g)  I think that will give us a deformation that is rotation-invariant.
-    deformGrid2d(fit$x, ww,
-                 ngrid=25, pch=19, main=paste(ste, i), gridcol = "black")
+    # On right: plot a deformation that is rotation-invariant wrt original lat/lon
+    deformGrid2d(fit$x, ww, ngrid=25, pch=19, main=paste(ste, i), gridcol = "black")
   }
 }
 
