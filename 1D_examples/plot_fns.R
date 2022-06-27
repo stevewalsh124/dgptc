f7fd6d667cb152403c60100400a9b0146612a88d
Steve Walsh <walsh124@vt.edu>
2022-06-27 10:16:29 -0400

    add functions plot.krig and plot.true

--- 1D_examples/plot_fns.R
+++ 1D_examples/plot_fns.R
@@ -36,3 +36,103 @@ plot.warp <- function(fit, wl = 1, wh = length(fit$x), ref.scale = 1){
   lines(x, w_ub, col="blue", lty=2)
   abline(0, 1, col="red")
 }
+
+plot.krig <- function(fit, zz=fit$mean, Y=parent.frame()$Y, precs_pred=parent.frame()$precs_pred){
+  
+  rg <- range(c(zz-2*sqrt(fit$s2_smooth*mean(fit$tau2)*mean(fit$g)),
+                zz+2*sqrt(fit$s2_smooth*mean(fit$tau2)*mean(fit$g)),
+                zz-2*sqrt(fit$s2*mean(fit$tau2)),
+                zz+2*sqrt(fit$s2*mean(fit$tau2)),
+                zz-2*sqrt(1/precs_pred),
+                zz+2*sqrt(1/precs_pred), Y))
+  
+  plot(fit$x_new, zz, type="l", col="blue", lwd=1.5,
+       ylim = rg, main = "fit")
+  for (i in 1:nrow(Y))  lines(c(x), Y[i,], col="gray")#-y_avg)
+  lines(fit$x_new, zz, col="blue", lwd=1.5)
+  lines(fit$x_new, zz-2*sqrt(fit$s2_smooth), col=2, lwd=1.5)
+  lines(fit$x_new, zz+2*sqrt(fit$s2_smooth), col=2, lwd=1.5)
+  lines(fit$x_new, zz-2*sqrt(fit$s2), col=2, lwd=1.5)
+  lines(fit$x_new, zz+2*sqrt(fit$s2), col=2, lwd=1.5)
+  lines(fit$x_new, zz-2*sqrt(1/precs_pred), col=3, lwd=1.5)
+  lines(fit$x_new, zz+2*sqrt(1/precs_pred), col=3, lwd=1.5)
+  lines(fit$x_new, zz-2*sqrt(1/(nrun*precs_pred)), col=3, lwd=1.5)
+  lines(fit$x_new, zz+2*sqrt(1/(nrun*precs_pred)), col=3, lwd=1.5)
+  lines(fit$x, y_hi, lwd=1.5, lty=2)
+  legend("topright", col=c("blue",2:3,1), legend = c("mean","UQ", "precs (low res)","true S"), 
+         lty=c(1,1,1,2), lwd=1.5)
+}
+
+# Plot simulations of truth given the data
+plot.true <- function(fit, S_e = fit$Sigma_hat){
+  S_ei <- matrix.Moore.Penrose2(S_e)
+  S_ei <- (S_ei+t(S_ei))/2
+  
+  Cs <- matrix(NA, length(fit$x)^2, fit$nmcmc)
+  Ss <- St <- Sw <- Sx <- Ms <- matrix(NA, length(fit$x), fit$nmcmc)
+  for (i in 1:fit$nmcmc){
+    if( i %% 2500 == 0) print(i)
+    theta <- fit$theta_y[i]
+    # tau2 <- fit$tau2[i]
+    # t2S_ei <- 1/tau2 * S_ei
+    g <- fit$g[i]
+    W <- fit$w[[i]]
+    dw <- sq_dist(W)
+    if(v==999){
+      S_si <- solve(Exp2Fun(distmat = dw, covparms = c(1, theta, g))) #1/tau2 * 
+    } else {
+      S_si <- solve(MaternFun(distmat = dw, covparms = c(1, theta, g, v))) #1/tau2 * 
+    }
+    S_si <- (S_si+t(S_si))/2
+    C <- Cs[,i] <- solve(S_si + S_ei)
+    C <- (C+t(C))/2
+    M <- Ms[,i] <- C %*% S_ei %*% fit$y
+    if(i %% 1000 ==0) print(range(M))
+    Ss[,i] <- M + matrix.sqrt(C)%*%rnorm(length(fit$y))
+    # St[,i] <- mvtnorm::rmvnorm(n = 1, mean = M, sigma = C, method = "eigen")
+    # Sw[,i] <- mvtnorm::rmvnorm(n = 1, mean = M, sigma = C, method = "svd")
+    # Sx[,i] <- mvtnorm::rmvnorm(n = 1, mean = M, sigma = C, method = "chol")
+  }
+  
+  m <- rowMeans(Ss) #- y_avg
+  ub <- apply(Ss, 1, function(x){quantile(x,0.975)}) #- y_avg
+  lb <- apply(Ss, 1, function(x){quantile(x,0.025)}) #- y_avg
+  ubb <- apply(Ss, 1, function(x){quantile(x,0.995)}) #- y_avg
+  lbb <- apply(Ss, 1, function(x){quantile(x,0.005)}) #- y_avg
+  
+  emp_cover <- round(mean(y_hi > lb & y_hi < ub),3)
+  emp_cover99 <- round(mean(y_hi > lbb & y_hi < ubb),3)
+  
+  plot(fit$x, fit$y, type="n",
+       ylim = range(c(m, lb, ub, lbb, ubb, Y)), 
+       main = paste0("est both,",if(taper_cov){ paste0("taper error w bohman\n",
+                tau_b,"=tau\n")}, emp_cover, " ", emp_cover99))
+  
+  for (i in 1:nrun) lines(fit$x, Y[i,], col="gray")
+  lines(fit$x, y_hi, lwd=1.5, col="red")
+  lines(fit$x, fit$y, lwd=1.5, lty=2)
+  lines(fit$x, m , col="blue")
+  lines(fit$x, lb , col="blue", lty=2)
+  lines(fit$x, ub , col="blue", lty=2)
+  lines(fit$x, lbb , col="darkblue", lty=2)
+  lines(fit$x, ubb , col="darkblue", lty=2)
+  legend("bottomright", legend = c("true", "sample avg", "UQ"), 
+         col=c("red","black", "blue"), lty=c(1,2,1), lwd=c(2,2,1))
+  
+  # Using law of total expectation and variance instead
+  mu_bar <- rowMeans(Ms)
+  C_bar <- matrix(rowMeans(Cs),  length(fit$x),  length(fit$x))
+  cov_mut <- cov(t(Ms))
+  
+  plot(x, mu_bar, type="n", main = "law of total E & V; MPI version", col="blue",
+       ylim=range(Y))
+  for (i in 1:nrun) lines(fit$x, Y[i,], col="gray")
+  lines(x, mu_bar, col="blue")
+  lines(x, y_hi, col="red")
+  lines(x, fit$y, lty=2)
+  lines(x, mu_bar - 2*sqrt(diag(C_bar+cov_mut)), col="blue")
+  lines(x, mu_bar + 2*sqrt(diag(C_bar+cov_mut)), col="blue")
+  legend("bottomright", legend = c("true", "sample avg", "UQ"), 
+         col=c("red","black", "blue"), lty=c(1,2,1), lwd=c(2,2,1))
+  
+}
