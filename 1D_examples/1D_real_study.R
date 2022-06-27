d51d30c985b8f92f334a57e025c36b35c8db4d33
Steve Walsh <walsh124@vt.edu>
2022-06-27 10:33:57 -0400

    init commit cosmology eg with tapering

---
+++ 1D_examples/1D_real_study.R
@@ -0,0 +1,126 @@
+# real data study
+
+one_layer <- F
+if(one_layer) {source("logl_cov_1L.R")} else {source("logl_cov.R")}
+
+source("matrix.Moore.Penrose.R")
+source("plot_fns.R") #plot.krig, plot.true, plot.warp
+
+# load the precision data (k, prec_highres, prec_lowres, index_list)
+load("Mira-Titan-IV-data/precision_and_indexes.Rdata")
+precs <- prec_lowres[index_list$lowres.ix]
+
+library(MASS) #ginv
+library(fields) #image.plot
+library(mvtnorm) #rmvnorm
+library(plgp) #distance (which is squared distances)
+
+# Taper the covariance matrix?
+taper_cov <- T
+
+# Do a kriging step?
+krig <- F
+
+ncores <- 6
+seed <- 1
+
+bohman <- function(t, tau = 0.25){
+  boh <- (1-t/tau)*cos(pi*t/tau)+sin(pi*t/tau)/pi
+  boh <- ifelse(t>=tau, 0, boh)
+}
+
+cov_fn <- "matern"#"exp2"#
+
+tau_b <- 0.1
+nrun <- 16
+nmcmc <- 7500
+nburn <- 1500
+kth <- 2
+
+bte <- 3 # cols 3-18 are low res
+
+args <- commandArgs(TRUE)
+if(length(args) > 0)
+  for(i in 1:length(args))
+    eval(parse(text=args[[i]]))
+
+pdf(paste0("pdf/realstudydgpact_",nmcmc,"_",nrun,"_",
+           if(taper_cov){paste0("tpr")},tau_b,
+           if(one_layer){"_1L"},"_",cov_fn,"_",seed,".pdf"))
+
+step <- 499
+
+# Model 1, choose from 000-111
+i <- 1
+
+# first col is k, 2nd is linear pert theory, 3:18 is lowres, 19 is hires
+pk2 <- read.table(paste0("Mira-Titan-IV-data/Mira-Titan-2021/STEP",step,"/pk_M",
+                         if(i<100){"0"},if(i<10){"0"},i,"_test.dat"))
+
+# subtract a linear model
+temp_avg <- rowMeans(log10(pk2[index_list$lowres.ix,3:18]))
+temp_lm <- lm(temp_avg ~ log10(pk2[index_list$lowres.ix,1]))$fitted.values
+Y <- t(apply(t(log10(pk2[index_list$lowres.ix,3:18])),1,function(x){x-temp_lm}))
+
+# wavenumber is X, a particular lowres run in Y
+x <- log10(k[index_list$lowres.ix])
+y <- log10(pk2[index_list$lowres.ix, bte]) - temp_lm
+y_avg <- rowMeans(log10(pk2[index_list$lowres.ix, 3:18]) - temp_lm)
+y_hi <- log10(pk2[index_list$lowres.ix, 19]) - temp_lm
+
+# hi res precs on the low res index (for comparison)
+precs_hi <- prec_highres[index_list$lowres.ix]
+
+# wavenumber is X, a particular lowres run in Y
+mean_sz <- mean(y_avg)
+sd_sz <- sd(y_avg)
+x <- log10(k[index_list$lowres.ix])
+x <- (x - min(x))/(max(x)-min(x))
+y <- (y - mean_sz)/sd_sz
+y_avg <- (y_avg - mean_sz)/sd_sz
+y_hi <- (y_hi - mean_sz)/sd_sz
+for (i in 1:nrow(Y)) { Y[i,] <- (Y[i,] - mean_sz)/sd_sz  }
+
+# get predictions for xx and have corresponding prec info
+xx <- setdiff(seq(0,1,by=.01), x)
+lmfit <- lm(log10(precs) ~ x) #precs for logl_g.R, 1/diag(covY) for logl_cov.R
+betahat <- coef(lmfit)
+precs_pred <- as.numeric(10^(cbind(1,xx) %*% betahat))
+
+Sigma_hat <- cov(Y)
+
+if(taper_cov) Sigma_hat <- Sigma_hat * bohman(plgp:::distance(x), tau=tau_b)
+
+par(mfrow=c(1,2))
+matplot(x,t(Y),type="l")
+lines(x,y_avg,col="red",lwd=2)
+lines(x,y_hi,col="yellow",lwd=2)
+image.plot(Sigma_hat/nrun, main = "input as sigma_hat")
+
+####################
+# run for sim data #
+####################
+
+fitcov <- fit_two_layer_SW(x = x, y = y_avg, nmcmc = nmcmc, Sigma_hat = Sigma_hat/nrun, cov = cov_fn)
+# plot(fitcov)
+fitcov <- trim_SW(fitcov, nburn, kth)
+plot(fitcov)
+if(krig) {
+  fitcov <- predict.dgp2_SW(object = fitcov, xx, cores=ncores, precs_pred = precs_pred)
+  
+  ## MASSIVE HACK
+  fitcov$s2 <- ifelse(fitcov$s2 < 0, 0, fitcov$s2)
+  fitcov$s2_smooth <- ifelse(fitcov$s2_smooth < 0, 0, fitcov$s2_smooth)
+  
+  par(mfrow=c(1,1))
+  plot.krig(fitcov)
+}
+v <- fitcov$v
+
+par(mfrow=c(1,2))
+plot.true(fitcov)
+if(!taper_cov) plot.true(fitcov, S_e = fitcov$Sigma_hat*bohman(plgp:::distance(x),tau = tau_b))
+
+if(!one_layer) plot.warp(fitcov)
+
+dev.off()
